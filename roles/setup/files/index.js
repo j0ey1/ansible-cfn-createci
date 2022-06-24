ed638ba7f0bed350c7bc15d1bacce41796834cbe
jose <josefelad>
2022-06-24 02:51:36 +0100

    edit index.js

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.end("Hello world! It's the badass DevOps Engineer again..");
+res.end("Hello world Its the badass DevOps Engineer again..");
 });
 server.listen(3000);
